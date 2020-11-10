ebb233b22ba4fd4f9ac211aa626b323a860806fe
jul-gold <julian.goldberg@columbia.edu>
2020-11-10 00:39:31 -0500

    Sign-up functionality

--- triAll/src/main/java/models/Trial.java
+++ triAll/src/main/java/models/Trial.java
@@ -3,4 +3,12 @@ package models;
 public class Trial {
 	
 	private int id;
+	private User researcher;
+	private Criteria crit;
+	
+	public Trial(int id, User r, Criteria crit) {
+		this.id = id;
+		researcher = r;
+		this.crit = crit;
+	}
 }
--- triAll/src/main/java/models/User.java
+++ triAll/src/main/java/models/User.java
@@ -3,7 +3,39 @@ package models;
 import java.util.LinkedList;
 
 public class User {
-	private String username;
+	private int id;
+	private float lat;
+	private float lon;
+	private String first;
+	private String last;
+	private String email;
 	private boolean isResearcher;
+	private Criteria data;
 	private LinkedList<Trial> trials;
+	
+	public User(int id, float lat, float lon, String first, String last, String email, boolean isR) {
+		this.id = id;
+		this.lat = lat;
+		this.lon = lon;
+		this.first = first;
+		this.last = last;
+		this.email = email;
+		isResearcher = isR;
+	}
+	
+	public int getID() {
+		return id;
+	}
+	
+	public void setData(Criteria data) {
+		if(!isResearcher) {
+		  this.data = data;
+		}
+	}
+	
+	public void addTrial(Trial t) {
+		if(isResearcher) {
+		  trials.add(t);
+		}
+	}
 }
