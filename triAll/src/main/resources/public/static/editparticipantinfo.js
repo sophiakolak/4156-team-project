5402d3c9c49e29a3e8bd004d38c379f7e0a9cc03
Sarah Hancock <sarah.hancock@columbia.edu>
2020-11-18 18:13:11 -0500

    Add metric and imperial to signup form

--- triAll/src/main/resources/public/static/editparticipantinfo.js
+++ triAll/src/main/resources/public/static/editparticipantinfo.js
@@ -14,6 +14,18 @@ $(document).ready(function(){
     	save_changes($( this ).serializeArray())
         event.preventDefault();
     });
+
+    $('input[type="radio"]').click(function() {
+       else if($(this).attr('id') == 'metricButton') {
+            $('.imperial').hide();
+            $('.metric').show()
+
+       }
+       else if($(this).attr('id') == 'imperialButton') {
+            $('.metric').hide(); 
+            $('.imperial').show();      
+       }
+   });
 })
 
 // Add function to load in participant info
@@ -83,6 +95,23 @@ function signOut() {
 }
 
 function save_changes(form_data){    
+    // Convert height to height in inches
+    // Convert weight to weight in pounds
+    var heightInInches;
+    var weightInLbs
+    if ($('#metricButton:checked').length > 0) {
+      // metric
+      weightInLbs = $('#kilograms').val() * 2.205
+      heightInInches = $('#centimeters').val() * (1/2.54)
+    } else {
+      // imperial
+      weightInLbs = $('#pounds').val()
+      heightInInches = (12 * $('#feet').val()) + Number($('#inches').val())
+    }
+
+    $("#heightInInches").val(heightInInches)
+    $("#weightInLbs").val(weightInLbs)
+    
     $.ajax({
         type: "POST",
         url: "/edit-part-submit",                
