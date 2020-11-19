532f40dad2791298616391007e85ed5ebf99eb49
jul-gold <julian.goldberg@columbia.edu>
2020-11-19 14:28:20 -0500

    Checkstyle changes

--- triAll/src/main/java/models/Match.java
+++ triAll/src/main/java/models/Match.java
@@ -2,21 +2,21 @@ package models;
 
 public class Match {
 
-	private int id;
-	private Trial trial;
-	//private User researcher;
-	//private User participant;
-	private double distance;
-	private String status;
-	
-	public Match(int id, Trial t, double d, String s) {
-		this.id = id;
-		trial = t;
-		distance = d;
-		status = s;
+  private int id;
+  private Trial trial;
+  //private User researcher;
+  //private User participant;
+  private double distance;
+  private String status;
+
+  public Match(int id, Trial t, double d, String s) {
+    this.id = id;
+    trial = t;
+    distance = d;
+    status = s;
 	}
-	
-	public double getDistance() {
-		return distance;
-	}
+
+  public double getDistance() {
+    return distance;
+  }
 }
