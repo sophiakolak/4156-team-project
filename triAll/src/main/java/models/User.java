cfda766a022298c0967e06c533ce51def117ec66
jul-gold <julian.goldberg@columbia.edu>
2020-11-21 22:43:36 -0500

    Update User.java

--- triAll/src/main/java/models/User.java
+++ triAll/src/main/java/models/User.java
@@ -131,6 +131,27 @@ public class User {
     Match m = new Match(id, t, distance, "pending");
     this.matches.add(m);
   }
+  
+  public int acceptMatch(int id) {
+    for (Match m : matches) {
+      if (m.getTrial().getID() == id) {
+        m.setStatus("accepted");
+        m.getTrial().confirmOne();
+        return m.getTrial().getPartConf();
+      }
+    }
+    return -1;
+  }
+  
+  public boolean rejectMatch(int id) {
+    for (Match m : matches) {
+      if (m.getTrial().getID() == id) {
+        m.setStatus("rejected");
+        return true;
+      }
+    }
+    return false;
+  }
 
   public boolean isResearcher() {
     return isResearcher;
@@ -163,16 +184,12 @@ public class User {
   /**
    * Sort matches.
    */
-  public LinkedList<Trial> sortedMatches(){
+  public LinkedList<Match> sortedMatches(){
     if (isResearcher) {
       return null;
-	}
-    matches.sort(distCompare);
-    LinkedList<Trial> t = new LinkedList<>();
-    for(Match m : matches) {
-    	t.add(m.getTrial());
     }
-    return t;
+    matches.sort(distCompare);
+    return matches;
   }
   
   /**
@@ -180,7 +197,7 @@ public class User {
    */
   public static double distance(double lat1, double lon1, 
 		  double lat2, double lon2, String unit) {
-		if ((lat1 == lat2) && (lon1 == lon2)) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
 			return 0;
 		} else {
 			double theta = lon1 - lon2;
