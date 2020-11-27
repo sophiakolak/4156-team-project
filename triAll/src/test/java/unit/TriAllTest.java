4e7fa4da438347632478cf016bfc1cd463958f5f
Sophia Kolak <sophiakolak.sk@gmail.com>
2020-11-27 13:11:15 -0500

    updated parameters for notification in tests, added two new tests

--- triAll/src/test/java/unit/TriAllTest.java
+++ triAll/src/test/java/unit/TriAllTest.java
@@ -4,6 +4,7 @@ import kong.unirest.GetRequest;
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import kong.unirest.json.JSONObject;
+import controllers.TriAll;
 import models.Criteria;
 import models.Match;
 import models.Notification;
@@ -47,45 +48,52 @@ public class TriAllTest {
 	Trial t = new Trial(1, r, "cool trial", 0, 0, "Siberia", "2020-12-01", "2020-12-02", 12, 123, 100, 0, d);
 	
 	@Test 
+	@Order(1)
 	public void testMatches() {
 		Criteria c = new Criteria(1, 1, 23, 23, 67, 67, 130, 130, "male", "cool", "cool");
 		boolean is_match = d.matches(c);
 		assertEquals(true, is_match);		
 	}
 	
-/*	@Test 
+@Test 
+@Order(2)
 	public void testPartEmail() {
-		Notification n = new Notification("15:04:05Z07:00", u, t);
+		Notification n = new Notification("shirishIsCool@gmail.com", "res@res.com");
 		String part_email = n.getPartEmail();
 		assertEquals("shirishIsCool@gmail.com", part_email);
 	}
 	
 	@Test 
+	@Order(3)
 	public void testResEmail() {
-		Notification n = new Notification("15:04:05Z07:00", u, t);
+		Notification n = new Notification("shirishIsCool@gmail.com", "res@res.com");
 		String res_email = n.getResEmail();
-		assertEquals("", res_email);
+		assertEquals("res@res.com", res_email);
 	}
-*/	
+	
 	@Test 
+	@Order(4)
 	public void testUpdate() {
 		u.update(0, 0, "Shirish", "Shirish", "shirishIsNotCool@gmail.com");
 		assertEquals("shirishIsNotCool@gmail.com", u.getEmail());
 	}
 	
 	@Test
+	@Order(5)
 	public void addTrial() {
 		r.addTrial(1, t);
 		assertEquals(r.getTrial(1).getID(),1);
 	}
 	
 	@Test
+	@Order(6)
 	public void containsTrial() {
 		r.addTrial(1, t);
 		assertEquals(r.containsTrial(1), true);
 	}
 	
 	@Test 
+	@Order(7)
 	public void sortedMatches() {
 		 Trial t2 = new Trial(2, r, "cooler trial", 1, 1, "Siberia", "2020-12-01", "2020-12-02", 12, 123, 100, 0, d);
 		 u.addMatch(1, t);
@@ -93,7 +101,8 @@ public class TriAllTest {
 		 assertEquals(u.sortedMatches().get(0).getID(),1);
 	}
 	
-	@Test 
+	@Test
+	@Order(8)
 	public void sortedTrials() {
 		Trial t1 = new Trial(2, r, "even cooler trial", 1, 1, "NYC", "2020-12-01", "2020-12-02", 12, 123, 100, 0, d);
 		Trial t2 = new Trial(3, r, "cooler trial", 1, 1, "Siberia", "2020-11-20", "2020-11-21", 12, 123, 100, 0, d);
@@ -102,11 +111,36 @@ public class TriAllTest {
 		assertEquals(r.sortedTrials().get(0).getID(), 3);
 	}
 	
-	@Test 
+	@Test
+	@Order(9)
 	public void distance() {
 		double dist = u.distance(0,0,0,0,"M");
 		assertEquals(dist, 0);
 	}
+	
+	@Test 
+	@Order(10)
+	public void authenticate() {
+	  boolean auth = TriAll.authenticate(",", "notRealEmail@fake.com");
+	  assertEquals(auth, false);
+	}
+	
+	/**@Test 
+ public void userMatches() {
+	  
+	} **/
+	
+	@Test
+	public void getEmail() {
+	  String email = TriAll.getEmail("realemail@aol.com");
+	  assertEquals(email, "realemail@aol.com");
+	}
+	
+	/**
+	@Test 
+	public void trialMatches() {
+	  
+	}**/
 	
 	
 	
