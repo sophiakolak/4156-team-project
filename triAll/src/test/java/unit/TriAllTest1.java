df4966cee15c0b409cf618c397783242299c6b0d
Sophia Kolak <sophiakolak.sk@gmail.com>
2020-12-06 19:37:07 -0500

    adding another test class

---
+++ triAll/src/test/java/unit/TriAllTest1.java
@@ -0,0 +1,30 @@
+package unit;
+import com.google.gson.Gson;
+import kong.unirest.GetRequest;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+import controllers.TriAll;
+import models.Criteria;
+import models.Match;
+import models.Notification;
+import models.User;
+import models.Trial;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class TriAllTest1 {
+  
+  @Test 
+  
+}
